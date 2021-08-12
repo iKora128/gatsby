cd6adf37f90622b1c3b309ab759f5187b820887a
nagashimadaichi <daichi.feynman@live.jp>
2021-08-12 17:01:28 +0900

    bug fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -187,6 +187,6 @@ module.exports = {
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-force-trailing-slashes`,
         `gatsby-plugin-offline`,
-        'gatsby-plugin-dark-mode',
+        `gatsby-plugin-dark-mode`,
     ],
 }
--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -5,7 +5,7 @@ import { Link, StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
 import { Navigation } from '.'
-import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+import { ThemeToggler } from "gatsby-plugin-dark-mode"
 import config from '../../utils/siteConfig'
 
 // Styles
@@ -31,6 +31,8 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <style type="text/css">{`${site.codeinjection_styles}`}</style>
                 <body className={bodyClass} />
             </Helmet>
+            
+            {/* this styles dark-theme */}
             <div
                 style={{
                     backgroundColor: `var(--bg)`,
@@ -55,19 +57,24 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                     { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
                                     { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
                                     <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
+                                    
+                                    {/* darkmode toggle */}
                                     <ThemeToggler>
                                         {({ theme, toggleTheme }) => (
-                                            <label>
+                                            <div className="dark-button">
                                                 <input
                                                     type="checkbox"
-                                                    className="check-darkmode"
-                                                    onChange={e => toggleTheme(e.target.checked ? `dark` : `light`)}
-                                                    checked={theme === `dark`}
-                                                />{` `}
-                                                 Dark mode
-                                            </label>
+                                                    id="toggle"
+                                                    onChange={e =>
+                                                        toggleTheme(e.target.checked ? "dark" : "light")
+                                                    }
+                                                    checked={theme === "dark"}
+                                                />
+                                                <label for="toggle"></label>
+                                            </div>
                                         )}
                                     </ThemeToggler>
+
                                 </div>
                             </div>
                             { isHome ?
