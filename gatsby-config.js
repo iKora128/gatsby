1787c743effb1a00627eadc0de6c2f91e13e2b15
長嶋大地 <DAICHI@nagashimadaichi-no-MacBook-Pro.local>
2021-09-08 22:50:08 +0900

    youtube iframe css .kg-card iframe changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ try {
     }
 }
 
-if (process.env.NODE_ENV === `production` && config.siteUrl === `https://feynman-daichi.netlify.app` && !process.env.SITEURL) {
+if (process.env.NODE_ENV === `production` && config.siteUrl === `https://longisland3.com` && !process.env.SITEURL) {
     throw new Error(`siteUrl can't be localhost and needs to be configured in siteConfig. Check the README.`) // eslint-disable-line
 }
 
