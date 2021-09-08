a0b321017b97ca74a142f79be8c3277f205cdd95
長嶋大地 <DAICHI@nagashimadaichi-no-MacBook-Pro.local>
2021-09-08 23:59:19 +0900

    bux fix

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,19 +1,10 @@
 module.exports = {
-<<<<<<< HEAD
-    siteUrl: `http://localhost:8000`, // Site domain. Do not include a trailing slash!
-
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
-
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
-=======
     siteUrl: `https://longisland3.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `longisland3`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `粒の振る舞い, 自己増殖, そしてつながりと感情`, // This allows an alternative site description for meta data for pages.
->>>>>>> origin/master
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
